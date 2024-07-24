dfe2a12f198d92a0ccc5866ad2ae450e58a9e426
alps <43815725@qq.com>
2024-07-24 17:51:25 +0800

    :test: vercel test

--- src/pages/api/gateway.ts
+++ src/pages/api/gateway.ts
@@ -4,7 +4,7 @@ export const config = {
     runtime: 'edge',
 };
 
-const fun = async (req: Request) => {
+const fun = async (req: Request, res: Response) => {
   console.log('in gateway', process.env.NODE_ENV)
   if (process.env.NODE_ENV === 'development') {
     const { createGatewayOnEdgeRuntime } = await import('@lobehub/chat-plugins-gateway');
@@ -12,7 +12,7 @@ const fun = async (req: Request) => {
     return createGatewayOnEdgeRuntime()(req);
   }
 
-  return createGatewayOnNodeRuntime()(req);
+  return createGatewayOnNodeRuntime()(req, res);
 };
 
 export default fun
