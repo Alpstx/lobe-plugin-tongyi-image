3272aa3871fac198108ab6265d91ddb1ba5f4622
alps <43815725@qq.com>
2024-07-29 16:30:29 +0800

    chore: Update nest-cli.json to include assets and enable asset watching

--- src/main.ts
+++ src/main.ts
@@ -14,7 +14,7 @@ async function bootstrap() {
     credentials: true,
     maxAge: 3600,
   })
-  app.useStaticAssets('public', {
+  app.useStaticAssets('src/public', {
     prefix: '/static',
   })
   // app.use(new FileMiddleware().use)
